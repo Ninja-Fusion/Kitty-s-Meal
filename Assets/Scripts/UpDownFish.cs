d49f8d589840c3a07035e5fb24a476938db9e4f4
Fotis Aronis <aronisfotis@gmail.com>
2018-05-13 19:41:43 +0300

    first push

---
+++ Assets/Scripts/UpDownFish.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpDownFish : MonoBehaviour {
+    Vector3 fishPos;
+    private bool moveUpDown = true;
+    void Start()
+    {
+        fishPos = transform.position;
+    }
+
+    void Update()
+    {
+
+        if (moveUpDown)
+        {
+            if (transform.position.y <= fishPos.y + 0.05f)
+                transform.position = new Vector3(transform.position.x, transform.position.y + 1 * Time.deltaTime * 0.15f, transform.position.z);
+            else
+                moveUpDown = false;
+        }
+        else
+        {
+            if (transform.position.y >= fishPos.y - 0.05f)
+                transform.position = new Vector3(transform.position.x, transform.position.y - 1 * Time.deltaTime * 0.15f, transform.position.z);
+            else
+                moveUpDown = true;
+        }
+    }
+}
